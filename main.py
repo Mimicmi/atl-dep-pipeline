2cb69d8ba759aff2d7c946978ca3bc77498b66a0
Simon Frezard <simon.frezard@gmail.com>
2024-06-06 09:49:17 +0200

    feat(requirements, gitignore): added pandas in requirements

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 import json
 from pprint import pprint as pp
+import pandas as pd
 
 response = requests.get(
     "https://data.enedis.fr/api/explore/v2.1/catalog/datasets/coefficients-des-profils/records")
