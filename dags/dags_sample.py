39db9901a6736cd20c1074938ec0d8960855855c
LucasBrobro <116753164+LucasBrobro@users.noreply.github.com>
2024-06-06 11:31:23 +0200

    modif start_date

--- dags/dags_sample.py
+++ dags/dags_sample.py
@@ -16,7 +16,7 @@ def fetch_data():
 dag = DAG(
     'example_postgres_dag',  # nom du dag
     # date de départ (pour schedule_intervale)
-    start_date=datetime(2024, 1, 1),
+    start_date=datetime(2024, 6, 6),
     schedule_interval='@daily')  # définition de l'exécution automatique (ici tous les jours) -> en fonction de la date qu'on met dans start_date airflow va rattraper auto toutes les exécutions pas faites depuis la date
 
 fetch_data_task = PythonOperator(  # première task du dag
