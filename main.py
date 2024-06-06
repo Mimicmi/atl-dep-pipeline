198ef96d94dd2023c90503d29e4915eca94c067b
Simon Frezard <simon.frezard@gmail.com>
2024-06-06 10:51:36 +0200

    feat(antoine): added antoine merge, autopep

--- main.py
+++ main.py
@@ -4,21 +4,23 @@ from pprint import pprint as pp
 import pandas as pd
 
 # Config url appel api
-response_profil = requests.get("https://data.enedis.fr/api/explore/v2.1/catalog/datasets/coefficients-des-profils/records")
-response_temp = requests.get("https://data.enedis.fr//api/explore/v2.1/catalog/datasets/donnees-de-temperature-et-de-pseudo-rayonnement/records")
+response_profil = requests.get(
+    "https://data.enedis.fr/api/explore/v2.1/catalog/datasets/coefficients-des-profils/records")
+response_temp = requests.get(
+    "https://data.enedis.fr//api/explore/v2.1/catalog/datasets/donnees-de-temperature-et-de-pseudo-rayonnement/records")
 
 # transformation en json
-response_profil= response_profil.json()
-response_temp= response_temp.json()
+response_profil = response_profil.json()
+response_temp = response_temp.json()
 
 # stockage ans une variable
-records_profil=response_profil["results"]
+records_profil = response_profil["results"]
 records_temp = response_temp["results"]
 
 # création des tableaux qui vont recevoirs les données aprés les avoir trié
 timestamps_profil = []
-sous_profil=[]
-cp=[]
+sous_profil = []
+cp = []
 
 timestamps_temp = []
 trl = []
@@ -34,7 +36,6 @@ for record in records_temp:
     timestamps_temp.append(record["horodate"])
     trl.append(record["temperature_realisee_lissee_degc"])
     tnl.append(record["temperature_normale_lissee_degc"])
-
 
 
 # création du dataframe avec les bonnes données et les titres de colonne
